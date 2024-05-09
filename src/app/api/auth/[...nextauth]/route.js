fb33ae442acdac1a2e52b7506271ae28cff778e7
Vinicius Noronha <96258593+viniciusnxar@users.noreply.github.com>
2024-05-09 10:17:31 -0300

    update test

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from '@/libs/mongoConnect';
 import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
