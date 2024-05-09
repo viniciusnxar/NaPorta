fc69a818a713581607ba4966765d93ce1386d7b9
Vinicius Noronha <96258593+viniciusnxar@users.noreply.github.com>
2024-05-09 11:00:37 -0300

    update

--- src/components/layout/Hero.js
+++ src/components/layout/Hero.js
@@ -6,15 +6,15 @@ export default function Hero() {
     <section className="hero md:mt-4">
       <div className="py-8 md:py-12">
         <h1 className="text-4xl font-semibold">
-          Everything<br />
-          is better<br />
-          with a&nbsp;
+          lorem<br />
+          ipsum<br />
+          da&nbsp;
           <span className="text-primary">
-            Pizza
+            silva
           </span>
         </h1>
         <p className="my-6 text-gray-500 text-sm">
-          Pizza is the missing piece that makes every day complete, a simple yet delicious joy in life
+        Eum ipsam iste quos suscipit tempora? Aperiam esse fugiat inventore laboriosam officiis quam rem!
         </p>
         {/* <div className="flex gap-4 text-sm">
           <button className="flex justify-center bg-primary uppercase flex items-center gap-2 text-white px-4 py-2 rounded-full">
