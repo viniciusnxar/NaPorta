83f9163b33eb249a9ac3887d54bdd9017e7b959b
Vinicius Noronha <96258593+viniciusnxar@users.noreply.github.com>
2024-06-06 18:11:48 -0300

    Update

--- src/components/menu/AddToCartButton.js
+++ src/components/menu/AddToCartButton.js
@@ -1,4 +1,4 @@
-import FlyingButton from 'react-flying-item';
+// import FlyingButton from 'react-flying-item';
 
 export default function AddToCartButton({
   hasSizesOrExtras,
@@ -8,19 +8,19 @@ export default function AddToCartButton({
 }) {
   return (
     <div className='flying-button-parent mt-4'>
-      <FlyingButton targetTop={'1%'} targetLeft={'95%'} src={image}>
-        <button
-          type='button'
-          onClick={onClick}
-          className='bg-red-600 text-white rounded-md px-4 py-1 text-sm hover:bg-red-700 transition-all duration-200 ease-in-out'
-        >
-          {hasSizesOrExtras ? (
-            <span>Adicionar ao carrinho R${basePrice.toFixed(2)}</span>
-          ) : (
-            <span>Adicionar ao carrinho</span>
-          )}
-        </button>
-      </FlyingButton>
+      {/* <FlyingButton targetTop={'1%'} targetLeft={'95%'} src={image}> */}
+      <button
+        type='button'
+        onClick={onClick}
+        className='bg-red-600 text-white rounded-md px-4 py-1 text-sm hover:bg-red-700 transition-all duration-200 ease-in-out'
+      >
+        {hasSizesOrExtras ? (
+          <span>Adicionar ao carrinho R${basePrice.toFixed(2)}</span>
+        ) : (
+          <span>Adicionar ao carrinho</span>
+        )}
+      </button>
+      {/* </FlyingButton> */}
     </div>
   );
 }
